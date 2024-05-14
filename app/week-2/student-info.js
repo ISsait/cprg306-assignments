9c0f1bea665325e68086bb3f9fa4d73fed9e9ba3
ISsait <152314143+ISsait@users.noreply.github.com>
2024-05-14 12:38:34 -0600

    90% complete
    Just need to double check work

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    const linkStyle = "underline text-cyan-600 hover:text-cyan-300";
+    return (
+      <div>
+        <h1>Student Information: Ian Stoesz</h1>
+        <p>GitHub Repo URL: <Link className={linkStyle} href="https://github.com/ISsait/cprg306-assignments.git">ISsait</Link></p>        
+      </div>
+    );
+}
