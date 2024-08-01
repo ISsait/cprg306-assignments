7951a73d46fb42bf4406982a302bc4c3b6ea8040
ISsait <152314143+ISsait@users.noreply.github.com>
2024-08-01 13:51:01 -0600

    started week 10 assignment

---
+++ app/week-10/page.js
@@ -0,0 +1,76 @@
+"use client";
+
+import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
+
+export default function SignInPage() {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  async function handleSignIn() {
+    try {
+      await gitHubSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleSignOut() {
+    try {
+      await firebaseSignOut();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //console.dir(user); // sensitive user data
+
+  return (
+    <div>
+      <h1 className="text-2xl font-extrabold text-center m-10">
+        Shopping List Authentication Page
+      </h1>
+      {user ? (
+        // user is logged in
+        <div className="flex flex-col items-center">
+          <p>Welcome {user.email}</p>
+          <img
+            className="w-12 h-12 rounded-full m-2"
+            src={user.photoURL}
+            alt="User avatar"
+          />
+          <span className="text-lg bg-green-600 hover:bg-green-300 hover:underline border-solid border-2 border-yellow-300 rounded-md m-2 p-2">
+            <Link href="./week-8/shopping-list/">Go To Your Shopping List</Link>
+          </span>
+          <span>
+            <button
+              onClick={handleSignOut}
+              className="text-lg border-solid border-2 border-blue-950 rounded-md bg-blue-800 hover:underline hover:bg-blue-500 p-2 m-2"
+            >
+              Sign Out
+            </button>
+          </span>
+        </div>
+      ) : (
+        // user is not logged in
+        <div className="flex flex-col items-center">
+          <button
+            onClick={handleSignIn}
+            className="text-lg border-solid border-2 border-blue-950 rounded-md bg-blue-800 hover:underline hover:bg-blue-500 p-2 m-2"
+          >
+            Sign In
+          </button>
+        </div>
+      )}
+      <footer className="flex flex-col items-center fixed bottom-10 w-full">
+        <p>
+          <Link
+            className="border-solid border-2 border-blue-950 rounded-md bg-blue-800 hover:underline hover:bg-blue-500 p-2 m-2"
+            href="./"
+          >
+            Home
+          </Link>
+        </p>
+      </footer>
+    </div>
+  );
+}
---
+++ app/week-10/shopping-list/page.js
@@ -0,0 +1,65 @@
+"use client";
+
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import { useState } from "react";
+import itemsData from "./items.json";
+import Link from "next/link";
+import { useUserAuth } from "../_utils/auth-context";
+
+export default function Page() {
+  const linkStyle = "underline text-cyan-600 hover:text-cyan-300";
+  const modImportedItems = itemsData.map((item) => ({
+    ...item,
+    selected: false,
+  }));
+  const [items, setItems] = useState(modImportedItems);
+
+  const handleAddItem = (newItem) => {
+    setItems((prevItems) => [...prevItems, newItem]);
+    console.log("updated items", items);
+  };
+
+  const handleClick = (item) => {
+    const updatedItems = items.map((i) => {
+      if (i.id === item.id) {
+        return { ...i, selected: !i.selected };
+      }
+      return i;
+    });
+
+    setItems(updatedItems);
+  };
+
+  const { user } = useUserAuth();
+  console.dir(user); // sensitive user data
+  return (
+    <main className="m-4">
+      {user ? (
+        <div>
+          <span className="grid grid-cols-2">
+            <p>Logged in as: {user.email}</p>
+          </span>
+          <h1 className="p-3 text-3xl font-serif border">Shopping List</h1>
+          <div>
+            <NewItem onAddItem={handleAddItem} />
+          </div>
+          <div>
+            <ItemList items={items} onHandleClick={handleClick} />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p>You must be logged in to view this page</p>
+          <Link href="/week-8/">Click here to return to the sign in page</Link>
+        </div>
+      )}
+
+      <div>
+        <Link className={linkStyle} href="./">
+          Back to sign in page
+        </Link>
+      </div>
+    </main>
+  );
+}
